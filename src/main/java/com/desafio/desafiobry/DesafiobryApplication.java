d3d6f568e919dc3f286676d4ff7e0f21c77ca31c
gabriel-s-flores <gabriel.soares27697@gmail.com>
2022-08-04 17:01:07 -0300

    set bouncy castle as security provider

--- src/main/java/com/desafio/desafiobry/DesafiobryApplication.java
+++ src/main/java/com/desafio/desafiobry/DesafiobryApplication.java
@@ -1,43 +1,42 @@
 package com.desafio.desafiobry;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.security.KeyStore;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DesafiobryApplication {
 
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
 		SpringApplication.run(DesafiobryApplication.class, args);
+
+		Security.addProvider(new BouncyCastleProvider());
 		
 
-        final InputStream doc = DesafiobryApplication.class.getClassLoader().getResourceAsStream("arquivos/doc.txt");
-        //final InputStream signatures = DesafiobryApplication.class.getClassLoader().getResourceAsStream("pkcs12/Desafio Estagio Java.p12");
-
+		final InputStream doc = DesafiobryApplication.class.getClassLoader().getResourceAsStream("arquivos/doc.txt");
+	
 
 		String txt = IOUtils.toString(doc, "utf8");
 
 		System.out.println(txt);
 
-		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		MessageDigest digest = MessageDigest.getInstance("SHA-256", "BC");
 
 		byte[] hash = digest.digest(txt.getBytes(StandardCharsets.UTF_8));
 
-		String sha256 = new String (Hex.encode(hash));
+		String sha256 = new String(Hex.encode(hash));
 
 		System.out.println(sha256);
-
-		
 
 	}
 
