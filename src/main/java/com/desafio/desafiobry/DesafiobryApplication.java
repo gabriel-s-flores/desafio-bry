519d0599098c786696694739da01afda0a040514
gabriel-s-flores <gabriel.soares27697@gmail.com>
2022-08-04 11:30:26 -0300

    commit inicial

---
+++ src/main/java/com/desafio/desafiobry/DesafiobryApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/desafiobry/DesafiobryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
